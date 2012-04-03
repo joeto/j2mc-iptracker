717110f002465d13da2560779b0d6e62c1ce1747
Matt Baxter <matt@phozop.net>
2012-04-02 23:39:33 -0400

    Formatting

--- src/to/joe/j2mc/iptracker/J2MC_IPtracker.java
+++ src/to/joe/j2mc/iptracker/J2MC_IPtracker.java
@@ -13,50 +13,50 @@ import org.bukkit.plugin.java.JavaPlugin;
 import to.joe.j2mc.core.J2MC_Manager;
 import to.joe.j2mc.iptracker.command.IPLookupCommand;
 
-public class J2MC_IPtracker extends JavaPlugin implements Listener{
+public class J2MC_IPtracker extends JavaPlugin implements Listener {
 
     @Override
-    public void onEnable(){
-        this.getServer().getPluginManager().registerEvents(this, this);
-        this.getCommand("iplookup").setExecutor(new IPLookupCommand(this));
-        
-        this.getLogger().info("IP tracker module enabled");
-    }
-    
-    @Override
-    public void onDisable(){
+    public void onDisable() {
         this.getLogger().info("IP tracker module disabled");
     }
-    
+
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+        this.getCommand("iplookup").setExecutor(new IPLookupCommand(this));
+
+        this.getLogger().info("IP tracker module enabled");
+    }
+
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlayerPreLogin(PlayerPreLoginEvent event){
+    public void onPlayerPreLogin(PlayerPreLoginEvent event) {
         final String ip = event.getAddress().getHostAddress();
         final String name = event.getName();
-        try{
-            PreparedStatement ps = J2MC_Manager.getMySQL().getFreshPreparedStatementHotFromTheOven("SELECT * FROM alias WHERE Name=? AND IP=?");
+        try {
+            final PreparedStatement ps = J2MC_Manager.getMySQL().getFreshPreparedStatementHotFromTheOven("SELECT * FROM alias WHERE Name=? AND IP=?");
             ps.setString(1, name);
             ps.setString(2, ip);
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()){
+            final ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
                 int count = rs.getInt("Logins");
                 count++;
-                PreparedStatement increment = J2MC_Manager.getMySQL().getFreshPreparedStatementHotFromTheOven("UPDATE alias SET logins=?, Time=now() WHERE Name=? AND IP=?");
+                final PreparedStatement increment = J2MC_Manager.getMySQL().getFreshPreparedStatementHotFromTheOven("UPDATE alias SET logins=?, Time=now() WHERE Name=? AND IP=?");
                 increment.setInt(1, count);
                 increment.setString(2, name);
                 increment.setString(3, ip);
                 increment.executeUpdate();
-            }else{
-                PreparedStatement insertIntoAlias = J2MC_Manager.getMySQL().getFreshPreparedStatementHotFromTheOven("INSERT INTO alias (`Name`,`IP`,`Time`,`Logins`) VALUES(?,?,now(),?)");
+            } else {
+                final PreparedStatement insertIntoAlias = J2MC_Manager.getMySQL().getFreshPreparedStatementHotFromTheOven("INSERT INTO alias (`Name`,`IP`,`Time`,`Logins`) VALUES(?,?,now(),?)");
                 insertIntoAlias.setString(1, name);
                 insertIntoAlias.setString(2, ip);
                 insertIntoAlias.setInt(3, 1);
                 insertIntoAlias.executeUpdate();
             }
-        }catch(SQLException e){
+        } catch (final SQLException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (final ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
-    
+
 }
